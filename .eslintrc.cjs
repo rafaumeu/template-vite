643d190100c281a4147c02496ed5abc9afc7b706
Rafael Dias Zendron <mmmarckos@gmail.com>
2024-01-04 08:10:49 -0300

    feat: eslint
    adicionadas novas rules e  instaladas dependencias para q funcionem

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,12 +10,37 @@ module.exports = {
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
   settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh', 'prettier'], // Adiciona o plugin do Prettier
+  plugins: ['react-refresh', 'prettier', 'import', 'eslint-plugin-import-helpers'], // Adiciona o plugin do Prettier
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
     'prettier/prettier': 'error', // Ativa a regra do Prettier
+    'react/jsx-filename-extension': ['warn', { extensions: ['.js', '.jsx'] }],
+    'import/prefer-default-export': 'off',
+    'react/prefer-stateless-function': 'off',
+    'react/state-in-constructor': 'off',
+    'react/self-closing-comp': [
+      'error',
+      {
+        component: true,
+        html: true,
+      },
+    ],
+    'no-param-reassign': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'import-helpers/order-imports': [
+      'warn',
+      { // example configuration
+          newlinesBetween: 'always',
+          groups: [
+              'module',
+              '/^@shared/',
+              ['parent', 'sibling', 'index'],
+          ],
+          alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
   },
 };
